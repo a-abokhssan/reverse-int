fa3e24ad8d07bff735f9dfd02881a8ae58657254
a-abokhssan <a.abokhssan@gmail.com>
2021-03-14 17:26:41 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return parseInt(n.toString().split("").reverse().join(""));
+};
+
+/* If we need loop
+
+    let reversedNumber = 0;
+    let reminder = 0;
+    let modulusOfN = n > 0 ? n : -n;
+
+    while (modulusOfN > 0) {
+        reminder = modulusOfN % 10;
+        reversedNumber = reversedNumber * 10 + reminder;
+        modulusOfN = Math.floor(modulusOfN / 10);
+    }
+
+    return reversedNumber;
+*/
